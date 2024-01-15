3f5a1c401327df7652bcf2c2fde2bf90d0d12ddd
ChazzaT18 <charlie.tahsin123@gmail.com>
2024-01-15 13:47:29 +0000

    task 2 complete

--- controllers/topics-controller.js
+++ controllers/topics-controller.js
@@ -1,7 +1,6 @@
 const { fetchTopics } = require('../models/topics-model')
 
 const getTopics = ((request, response) => {
-    console.log(request)
     fetchTopics().then((topic) => {
         response.status(200).send({ topic: topic });
       });
