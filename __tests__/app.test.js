0331d6b59df1dd01831b1acfdd566f289c012883
ChazzaT18 <charlie.tahsin123@gmail.com>
2024-01-16 00:07:17 +0000

    5-GET/api/articles complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -19,7 +19,7 @@ describe("GET/api/topics", () => {
       .get("/api/topics")
       .then((response) => {
         const topics = response.body.topic;
-        expect(response.status).toBe(200);
+        expect(200);
         expect(Array.isArray(topics)).toBe(true);
       });
   });
@@ -32,7 +32,7 @@ describe("GET/api/topics", () => {
           expect(topic.hasOwnProperty("slug"));
           expect(topic.hasOwnProperty("description"));
         });
-        expect(response.status).toBe(200);
+        expect(200);
       });
   });
 });
@@ -42,7 +42,7 @@ describe("GET/api", () => {
       .get("/api")
       .then((response) => {
         const endPoints = response.body.endPoints;
-        expect(response.status).toBe(200);
+        expect(200);
         expect(typeof endPoints).toEqual("object");
       });
   });
@@ -51,53 +51,47 @@ describe("GET/api", () => {
       .get("/api")
       .then((response) => {
         const endPoints = response.body.endPoints;
-        expect(response.status).toBe(200);
+        expect(200);
         expect(endPoints).toEqual(endPointData);
       });
   });
 });
 describe("GET/api/articles/:article_id", () => {
-  test("returns status 200 and an object", () => {
-    return request(app)
-      .get("/api/articles/1")
-      .then((response) => {
-        const article = response.body.article;
-        expect(response.status).toBe(200);
-        expect(typeof article).toEqual("object");
-      });
-  });
   test("returns status 200 and an article object with correct properties", () => {
     return request(app)
       .get("/api/articles/1")
       .then((response) => {
         const article = response.body.article;
-        expect(response.status).toBe(200);
-        expect(article).toEqual({ article: {
-          article_id: 1,
-          title: "Living in the shadow of a great man",
-          topic: "mitch",
-          author: "butter_bridge",
-          body: "I find this existence challenging",
-          created_at: "2020-07-09T20:11:00.000Z",
-          votes: 100,
-          article_img_url:
-            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-        }});
-      });
-  })
-  test("returns status 404 when id number is not in the database", () => {
-    return request(app)
-      .get("/api/articles/10090")
-      .then((response) => {
-        expect(response.status).toBe(404);
-        expect(response.body.msg).toBe("Not found");
+        expect(typeof article).toEqual("object");
+        expect(200);
+        expect(article).toEqual({
+          article: {
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 100,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          },
+        });
       });
   });
   test("returns status 404 when id number is not in the database", () => {
     return request(app)
+      .get("/api/articles/10090")
+      .then((response) => {
+        expect(404);
+        expect(response.body.msg).toBe("Not found");
+      });
+  });
+  test("returns status 400 given an invalid article_id", () => {
+    return request(app)
       .get("/api/articles/banana")
       .then((response) => {
-        expect(response.status).toBe(400);
+        expect(400);
         expect(response.body.msg).toBe("article_id must be a number");
       });
   });
@@ -106,18 +100,51 @@ describe("GET/api/articles/:article_id", () => {
       .get("/api/articles/4")
       .then((response) => {
         const article = response.body.article;
-        expect(response.status).toBe(200);
-        expect(article).toEqual({ article:
-          { article_id: 4,
+        expect(200);
+        expect(article).toEqual({
+          article: {
+            article_id: 4,
             title: "Student SUES Mitch!",
             topic: "mitch",
             author: "rogersop",
             body: "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
-            "created_at": "2020-05-06T01:14:00.000Z",
+            created_at: "2020-05-06T01:14:00.000Z",
             article_img_url:
               "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
-              votes: 0
-          }})
+            votes: 0,
+          },
+        });
       });
-  })
+  });
+});
+describe("GET/api", () => {
+  test("returns status 200 and an object", () => {
+    return request(app)
+      .get("/api")
+      .then((response) => {
+        const endPoints = response.body.endPoints;
+        expect(200);
+        expect(typeof endPoints).toEqual("object");
+      });
+  });
+  test("returns 200 and an array of objects with the objects containing correct keys", () => {
+    return request(app)
+      .get("/api/articles")
+      .then((response) => {
+        const articles = response.body.articles;
+        expect(200);
+        expect(Array.isArray(articles)).toEqual(true);
+        expect(articles[0]).toEqual({
+          author: "icellusedkars",
+          title: "Eight pug gifs that remind me of mitch",
+          article_id: 3,
+          topic: "mitch",
+          created_at: "2020-11-03T09:12:00.000Z",
+          votes: 0,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: "2",
+        });
+      });
+  });
 });
--- controllers/articles-controller.js
+++ controllers/articles-controller.js
@@ -1,13 +1,24 @@
-const { fetchArticleById } = require('../models/articles-model')
+const { fetchArticleById, fetchArticles } = require("../models/articles-model");
 
 const getArticleById = (request, response, next) => {
-    const articleId = Number(request.params.article_id)
-    fetchArticleById(articleId).then((article) => {
-        response.status(200).send({article})
+  const articleId = Number(request.params.article_id);
+  fetchArticleById(articleId)
+    .then((article) => {
+      response.status(200).send({ article });
     })
     .catch((err) => {
-        next(err)
-    })
-}
+      next(err);
+    });
+};
 
-module.exports = { getArticleById };
+const getArticles = (request, response, next) => {
+  fetchArticles()
+    .then((articles) => {
+      response.status(200).send({articles});
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+module.exports = { getArticleById, getArticles };
