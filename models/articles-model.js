d567d57daa23d69378f2b9fce82591e07e2cda5a
ChazzaT18 <charlie.tahsin123@gmail.com>
2024-01-16 04:06:20 +0000

    task 6 complete /6-GET/api/articles/article_id/comments

--- models/articles-model.js
+++ models/articles-model.js
@@ -46,21 +46,26 @@ ORDER BY
   articles.created_at DESC`
     )
     .then((articles) => {
-      // articles.rows.forEach((article) => {
-      //   article.comment_count = Number(article.comment_count);
-      // });
-      console.log(articles.rows);
       return articles.rows;
     });
 };
 
 const fetchCommentsByArticleId = (articleId) => {
+  if (isNaN(articleId)) {
+    return Promise.reject({
+      statusCode: 400,
+      msg: "article_id must be a number",
+    });
+  }
   return db
     .query(
       "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC",
       [articleId]
     )
     .then((comments) => {
+      if (comments.rows.length === 0) {
+        return Promise.reject({ statusCode: 404, msg: "Not found" });
+      }
       return comments.rows;
     });
 };
